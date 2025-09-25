04637b4d6ffe43ee24a9579c2f32648e4f1a7f5c
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2025-09-24 23:21:39 -0300

    brand text update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import NavBar from './assets/sections/NavBar/NavBar'
 import About from './assets/sections/about/about'
+import Servicos from './assets/sections/servicos/servicos'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <NavBar />
       <About />
+      <Servicos />
     </>
   )
 }
--- src/assets/sections/about/about.jsx
+++ src/assets/sections/about/about.jsx
@@ -5,7 +5,7 @@ function About() {
         <>
             <div className="about">
                 <div className="title-clinica">
-                    <h2>PREMIER FEET CARE PODOLOGIA</h2>
+                    <h2>PREMIER FEET PODOLOGIA</h2>
                     <p>O melhor atendimento para <br />
                         seus pés!
                     </p>
@@ -17,13 +17,13 @@ function About() {
                 <div className="txt_about">
                     <h2>SOBRE A CLÍNICA</h2>
                     <p>
-                        Desde maio de 2023, a Premier Feet Care Podologia tem sido referência em saúde e bem-estar para os pés. Fundada com o propósito de oferecer um atendimento humanizado e altamente especializado, nossa clínica é liderada por uma podóloga experiente e apaixonada pelo que faz.
+                        Desde maio de 2023, a Premier Feet Podologia tem sido referência em saúde e bem-estar para os pés. Fundada com o propósito de oferecer um atendimento humanizado e altamente especializado, nossa clínica é liderada por uma podóloga experiente e apaixonada pelo que faz.
                     </p>
                     <p>
                         Nossa responsável técnica é especializada no tratamento de pés diabéticos, oferecendo um cuidado minucioso, seguro e totalmente personalizado para pacientes que necessitam de atenção especial. Com conhecimento técnico avançado e constante atualização na área, ela garante um atendimento que une ciência, prevenção e carinho em cada procedimento.
                     </p>
                     <p>
-                        Além do cuidado com pés diabéticos, a Premier Feet Care Podologia também oferece tratamentos completos para calos, unhas encravadas, fissuras, micose, hidratação profunda, entre outros. Tudo isso em um ambiente acolhedor, higienizado e com equipamentos de alta qualidade, pensado para proporcionar conforto e segurança.
+                        Além do cuidado com pés diabéticos, a Premier Feet Podologia também oferece tratamentos completos para calos, unhas encravadas, fissuras, micose, hidratação profunda, entre outros. Tudo isso em um ambiente acolhedor, higienizado e com equipamentos de alta qualidade, pensado para proporcionar conforto e segurança.
                     </p>
                     <p>
                         Venha nos conhecer e sinta a diferença de estar em boas mãos. Porque quando seus pés estão bem, todo o seu corpo agradece.
