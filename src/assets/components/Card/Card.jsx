f432501a63c41d5ade4d3a08e21c4f08a5cc9b3b
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2025-09-24 23:22:01 -0300

    init section servicos

---
+++ src/assets/components/Card/Card.jsx
@@ -0,0 +1,11 @@
+function Card({ txt }) {
+  return (
+    <div className="div-card">
+      <div className="card card-green">
+        <p>{txt}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/assets/sections/servicos/servicos.jsx
@@ -0,0 +1,24 @@
+import Card from '../../components/Card/Card';
+import './servicos.css';
+
+function Servicos() {
+    return (
+        <>
+            <div className="servicos-container">
+                <h2>Nossos Serviços</h2>
+            </div>
+
+            <Card txt="teste" />
+            <Card txt="teste" />
+            <Card txt="teste" />
+            <Card txt="teste" />
+            <Card txt="teste" />
+            <Card txt="teste" />
+            <Card txt="teste" />
+            <Card txt="teste" />
+            <Card txt="teste" />
+        </>
+    );
+}
+
+export default Servicos;
