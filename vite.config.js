ae8a4b9b1d81e89718cef3f5ef85ae9a2acbbcc5
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2025-09-28 15:09:31 -0300

    versão de teste

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/premier_feet_care_podologia/",
   server: {
     open: true
   }
