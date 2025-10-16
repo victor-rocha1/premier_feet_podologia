ab2c8f3b1f474f945900d5d876eefe3bcb8cbce6
Victor Rocha <145049490+victor-rocha1@users.noreply.github.com>
2025-10-16 13:35:51 -0300

    update footer.jsx - remoção de uma tag p

--- src/assets/sections/Footer/Footer.jsx
+++ src/assets/sections/Footer/Footer.jsx
@@ -11,16 +11,12 @@ function Footer() {
             <footer>
                 <div className="footer-container">
                     <div className="footer-content">
-
                         <div className="footer-logo">
                             <img src={logo} alt="Logo Premier Feet Podologia" />
                         </div>
-
                         <div className="copyright">
-                            <p>© 2025 Premier Feet Podologia. </p>
-                            <p>Todos os direitos reservados.</p>
+                            <p>© 2025 Premier Feet Podologia. Todos os direitos reservados.</p>
                         </div>
-
                         <div className="footer-contact">
                             <h4>Contato</h4>
                             <a href={whatsappLink} target="_blank" rel="noopener noreferrer">
@@ -30,7 +26,6 @@ function Footer() {
                                 <i className="bi bi-instagram"></i> premierfeet.podologia
                             </a>
                         </div>
-
                         <div className="footer-hours">
                             <h4>Horário de Funcionamento</h4>
                             <p><i className="bi bi-calendar3"></i> Quarta a Sábado</p>
